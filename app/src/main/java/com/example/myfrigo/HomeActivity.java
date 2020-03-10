d8b49407eab8bd66e85cd8bef16d6fd78713bbd6
Yusuf Mac <x18502983@student.ncirl.ie>
2020-03-10 15:14:33 +0000

    login

--- app/src/main/java/com/example/myfrigo/HomeActivity.java
+++ app/src/main/java/com/example/myfrigo/HomeActivity.java
@@ -2,13 +2,35 @@ package com.example.myfrigo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+    EditText mTextUsername;
+    EditText mTextPassword;
+    Button mButtonLogin;
+    TextView mTextViewRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        mTextUsername = (EditText) findViewById(R.id.edittext_username);
+        mTextPassword = (EditText) findViewById(R.id.edittext_password);
+        mButtonLogin = (Button) findViewById(R.id.button_login);
+        mTextViewRegister = (TextView) findViewById(R.id.textview_register);
+        mTextViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(HomeActivity.this,RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
+
     }
 }
