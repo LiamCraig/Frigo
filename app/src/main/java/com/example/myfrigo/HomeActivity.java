ceeabf36f478bd5e2a41651cf5d86dea1e716310
alanmellowes <x18467362@student.ncirl.ie>
2020-04-03 13:20:14 +0100

    functional login page

--- app/src/main/java/com/example/myfrigo/HomeActivity.java
+++ app/src/main/java/com/example/myfrigo/HomeActivity.java
@@ -4,16 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.TestLooperManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
-    EditText mTextUsername;
-    EditText mTextPassword;
-    Button mButtonLogin;
-    TextView mTextViewRegister;
+    private EditText mTextUsername;
+    private EditText mTextPassword;
+    private Button mButtonLogin;
+    private TextView mInfo;
+    private int counter = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,14 +25,29 @@ public class HomeActivity extends AppCompatActivity {
         mTextUsername = (EditText) findViewById(R.id.edittext_username);
         mTextPassword = (EditText) findViewById(R.id.edittext_password);
         mButtonLogin = (Button) findViewById(R.id.button_login);
-        mTextViewRegister = (TextView) findViewById(R.id.textview_register);
-        mTextViewRegister.setOnClickListener(new View.OnClickListener() {
+        mInfo = (TextView)findViewById(R.id.tvInfo);
+
+        mInfo.setText("No of attempts remaining: 5");
+
+        mButtonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Intent registerIntent = new Intent(HomeActivity.this,RegisterActivity.class);
-                startActivity(registerIntent);
+            public void onClick(View v) {
+                validate(mTextUsername.getText().toString(), mTextPassword.getText().toString());
             }
         });
+            }
+            private void validate(String usermTextUsername, String usermTextPassword){
+        if((usermTextUsername.equals("admin")) && (usermTextPassword.equals("123"))){
+            Intent intent = new Intent(HomeActivity.this, ContentActivity.class);
+            startActivity(intent);
+        }else{
+            counter--;
 
-    }
+            mInfo.setText("No of attempts remaining: " + String.valueOf(counter));
+
+            if(counter == 0){
+                mButtonLogin.setEnabled(false);
+            }
+        }
+            }
 }
