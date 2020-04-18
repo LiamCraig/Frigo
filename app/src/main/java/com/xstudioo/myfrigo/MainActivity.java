34eb678e89f266eac4942e962a0c95b436d8cc96
LiamCraig <liam-craig@hotnail.com>
2020-04-18 22:06:12 +0100

    list view added

--- app/src/main/java/com/xstudioo/myfrigo/MainActivity.java
+++ app/src/main/java/com/xstudioo/myfrigo/MainActivity.java
@@ -2,13 +2,23 @@ package com.xstudioo.myfrigo;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
@@ -18,6 +28,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.navigation.NavigationView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     Toolbar toolbar;
@@ -25,11 +37,23 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     ActionBarDrawerToggle toggle;
     NavigationView navigationView;
 
+    //array list for data
+    ArrayList<String> list = new ArrayList<>();
+    ListView list_view;
+    ArrayAdapter arrayAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+
+        //find view by id
+        list_view = findViewById(R.id.list_view);
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+        list_view.setAdapter(arrayAdapter);
+
 
         // get the list of stories titles and contents in string array
 
@@ -46,7 +70,132 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         toggle.syncState();
 
 
+
+        //liams added code(Implements List)
+        list_view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+
+                PopupMenu popupMenu = new PopupMenu(MainActivity.this, view);
+                popupMenu.getMenuInflater().inflate(R.menu.pop_up_menu, popupMenu.getMenu());
+
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+
+                        switch (item.getItemId()) {
+
+                            //UPDATE ITEM
+                            case R.id.item_update:
+
+                                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                                View v = LayoutInflater.from(MainActivity.this).inflate(R.layout.item_dialog, null, false);
+                                builder.setTitle("Update");
+                                final EditText editText = v.findViewById(R.id.etItem);
+                                editText.setText(list.get(position));
+
+                                //SETS CUSTOM VIEW FROM DIALOG
+                                builder.setView(v);
+
+                                builder.setPositiveButton("Update", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        if (!editText.getText().toString().isEmpty()) {
+                                            list.set(position, editText.getText().toString().trim());
+                                            arrayAdapter.notifyDataSetChanged();
+                                            Toast.makeText(MainActivity.this, "Item Updated!", Toast.LENGTH_SHORT).show();
+
+                                        } else {
+                                            editText.setError("add item here !");
+                                        }
+                                    }
+                                });
+
+                                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        dialog.dismiss();
+                                    }
+                                });
+
+                                builder.show();
+
+                                break;
+
+                            //DELETE ITEM
+                            case R.id.item_del:
+
+                                Toast.makeText(MainActivity.this, "Item Deleted", Toast.LENGTH_SHORT).show();
+                                list.remove(position);
+                                arrayAdapter.notifyDataSetChanged();
+
+                                break;
+
+                        }
+
+                        return true;
+                    }
+                });
+
+                popupMenu.show();
+
+            }
+        });
+
     }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            //FUNCTION TO ADD ITEMS
+            case R.id.add_item:
+
+                _addItem();
+                break;
+
+        }
+
+        return true;
+    }
+
+    //METHOD FOR ADDING ITEMS
+    private void _addItem() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Add New Item");
+
+        View v = LayoutInflater.from(MainActivity.this).inflate(R.layout.item_dialog, null, false);
+
+        builder.setView(v);
+        final EditText etItem = v.findViewById(R.id.etItem);
+        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                if (!etItem.getText().toString().isEmpty()) {
+                    list.add(etItem.getText().toString().trim());
+                    arrayAdapter.notifyDataSetChanged();
+
+                } else {
+                    etItem.setError("add item here !");
+                }
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.show();
+    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -74,8 +223,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         drawer.closeDrawer(GravityCompat.START);
         return true;
 
-       // if(menuItem.getItemId() == R.id.home){
-         //   Toast.makeText(this, "Home btn Clicked.", Toast.LENGTH_SHORT).show();
+        // if(menuItem.getItemId() == R.id.home){
+        //   Toast.makeText(this, "Home btn Clicked.", Toast.LENGTH_SHORT).show();
         //}
         //return true;
     }
