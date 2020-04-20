88c1c83febf947a15fd2b5b98bc349461c678381
Alan Mellowes <x18467362@student.ncirl.ie>
2020-04-20 02:32:40 +0100

    permission notification

--- app/src/main/java/com/xstudioo/myfrigo/ScanCodeActivity.java
+++ app/src/main/java/com/xstudioo/myfrigo/ScanCodeActivity.java
@@ -1,10 +1,18 @@
 package com.xstudioo.myfrigo;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
@@ -42,6 +50,27 @@ public class ScanCodeActivity extends AppCompatActivity implements ZXingScannerV
         super.onResume();
         ScannerView.setResultHandler(this);
         ScannerView.startCamera();
+        
+        requestForCamera();
+    }
+
+    private void requestForCamera() {
+        Dexter.withActivity(this).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse response) {
+                ScannerView.startCamera();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse response) {
+                Toast.makeText(ScanCodeActivity.this, "Camera permission is required", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                token.continuePermissionRequest();
+            }
+        }).check();
     }
 
 }
