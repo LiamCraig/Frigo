be57a581959461b587c88a1cd664eda13116caf1
LiamCraig <liam-craig@hotnail.com>
2020-04-20 22:19:10 +0100

    About Frigo added

--- app/src/main/java/com/xstudioo/myfrigo/scan.java
+++ app/src/main/java/com/xstudioo/myfrigo/scan.java
@@ -10,27 +10,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class scan extends AppCompatActivity {
 
-/*    //QR Scanner
-    public static TextView resultTextView;
-    Button btn_scan;
-    */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
 
 
-     /*   //QR Code Temporary Button + Code Item
-        resultTextView = (TextView) findViewById(R.id.result_txt);
-        btn_scan = (Button) findViewById(R.id.btn_scan);
 
-        btn_scan.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-
-                startActivity(new Intent(getApplicationContext(), ScanCodeActivity.class));
-            }
-        });*/
     }
 }
